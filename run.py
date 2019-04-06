bb4ef3ecf86f912f179628e1dd1090ac8d038c0d
gnence <45914618+gnence@users.noreply.github.com>
2019-04-06 17:17:56 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from app import app
 
-# pressed F5 for "Run" this
-# pressed Ctrl + S for "Rerun" this
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
-            port=5160,
+            port=5100,
             debug=True,
             threaded=True)
